e2efc3b6e24f4af90f7e88f43da5b55483a3a392
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-02-09 01:02:27 +0100

    tablesort and overview

--- src/build_mod_overview.py
+++ src/build_mod_overview.py
@@ -3,16 +3,25 @@ import json
 import urllib.request
 from mdutils.mdutils import MdUtils
 from mdutils.tools import Html
+from mdutils.tools.Link import Inline
 
 def build_mod_overview(mod_repo, cb):
     os.makedirs("docs/Mod Repository", exist_ok=True)
     mdModOverview = MdUtils(file_name='docs/Mod Repository/Overview', title='Overview')
-    mdModOverviewTable = ["Mod", "Description", "Version", "Language"]
+    mdModOverviewTable = ["Mod", "Type", "Description", "Version", "Language"]
     for key, value in mod_repo.items():
         mod = json.loads(urllib.request.urlopen(value["mod"].replace(" ", "%20")).read())
 
-        mdModOverviewTable.extend([mod["name"].replace("|", "&#124;"), mod["description"].replace("\n", "<br/>").replace("|", "&#124;"), mod["version"], "test"])
+        languages = [k for k, v in mod.items() if "translations" in v]
+
+        mdModOverviewTable.extend([
+            Inline.new_link('../' + mod["modType"] + '/' + mod["name"].replace("|", "&#124;"), text=mod["name"].replace("|", "&#124;")),
+            mod["modType"],
+            mod["description"].replace("\r\n", "<br/>").replace("\n", "<br/>").replace("|", "&#124;"),
+            mod["version"],
+            ", ".join(languages)
+        ])
 
         cb(value, mod)
-    mdModOverview.new_table(columns=4, rows=int(len(mdModOverviewTable)/4), text=mdModOverviewTable, text_align='center')
+    mdModOverview.new_table(columns=5, rows=int(len(mdModOverviewTable)/5), text=mdModOverviewTable, text_align='center')
     mdModOverview.create_md_file()
