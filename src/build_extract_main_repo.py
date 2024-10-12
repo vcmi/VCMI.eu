16d941c0248c69ac798415babe4bf2d57da2a50a
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-10-12 13:04:51 +0200

    new path for icons

--- src/build_extract_main_repo.py
+++ src/build_extract_main_repo.py
@@ -14,9 +14,9 @@ def build_extract_main_repo(data):
         shutil.copytree('additional/docs', 'docs', dirs_exist_ok=True)
 
         os.makedirs("docs/assets", exist_ok=True)
-        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/client/icons/vcmiclient.svg'), 'docs/assets/logo.svg')
-        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/client/icons/vcmiclient.svg'), 'docs/assets/logo2.svg')
-        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/client/icons/vcmiclient.16x16.png'), 'docs/assets/favicon.png')
+        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/clientapp/icons/vcmiclient.svg'), 'docs/assets/logo.svg')
+        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/clientapp/icons/vcmiclient.svg'), 'docs/assets/logo2.svg')
+        shutil.copy(os.path.join(tempdirname, 'vcmi-develop/clientapp/icons/vcmiclient.16x16.png'), 'docs/assets/favicon.png')
 
         shutil.copy(os.path.join(tempdirname, 'vcmi-develop/ChangeLog.md'), 'docs/ChangeLog.md')
         with open('docs/ChangeLog.md', "r") as file:
